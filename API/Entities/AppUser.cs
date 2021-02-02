641baabfcbc3d567b5b86750032819213e588c6a
Andrew Benner <abenner@hodgesmace.com>
2021-02-01 21:29:31 -0500

    End of section 9

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -12,7 +12,7 @@ namespace API.Entities
         public byte[] PasswordHash {get; set;}
         public byte[] PasswordSalt {get; set;}
 
-        public DateTime DateofBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created {get; set; } = DateTime.UtcNow;
         public DateTime LastActive { get; set; } = DateTime.UtcNow;
