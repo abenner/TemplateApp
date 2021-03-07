b3a05b3d3a9ab1774e65caf5c493222a46c8275f
Andrew Benner <abenner@hodgesmace.com>
2021-03-07 11:15:47 -0500

    End of section 16

---
+++ client/src/app/admin/admin-panel/admin-panel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
+
+@Component({
+  selector: 'app-admin-panel',
+  templateUrl: './admin-panel.component.html',
+  styleUrls: ['./admin-panel.component.css']
+})
+export class AdminPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
